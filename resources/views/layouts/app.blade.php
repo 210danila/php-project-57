abc4856be5bd6619d87f22058d5e2c53ffcf27f8
210danila <210danila@gmail.com>
2023-06-29 22:38:07 +0300

    Change title to TaskManager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Менеджер задач') }}</title>
+        <title>{{ config('app.name', 'TaskManager') }}</title>
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
