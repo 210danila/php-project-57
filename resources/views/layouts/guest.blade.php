01a7674c6d2c613238cfe5059b939baf6bfe4b68
210danila <210danila@gmail.com>
2023-06-30 03:48:11 +0300

    Update guest layout, change direct url to route

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                <h2 class="text-center"><a href="http://127.0.0.1:8000/">Менеджер задач</a></h2>
+                <h2 class="text-center"><a href="{{ route('root') }}">Менеджер задач</a></h2>
                 {{ $slot }}
             </div>
         </div>
