9a91fe81c221afd40cb436e2de953c4047c6f826
210danila <210danila@gmail.com>
2023-07-03 12:26:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,12 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/app.css'
             ],
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        assetsDir: '',
-        manifest: true,
-        minify: true,
-        sourcemap: false,
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-            },
-        },
-    },
     server: {
         https: true,
     },
